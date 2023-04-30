34d9f408bd70a4262550f1a56b7bbb6a1ccbc46e
adejolx <recursr.dev@gmail.com>
2023-04-30 19:44:20 +0100

    styles: add layout styling

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -12,54 +12,60 @@ const CountryDetails = () => {
   return (
     data && (
       <Wrapper>
-        <section>
+        <section className="stack-sm">
           <h2 className="sr-only">Country details</h2>
           <Wrapper className="cluster flex-start-x">
             <Link to="/" className="button button--link">
               <i className="fa-solid fa-arrow-left"></i> Back
             </Link>
           </Wrapper>
-          <Wrapper className="double-column">
+          <Wrapper className="switcher">
             <div>
               <img src={data[index]?.flags?.svg} />
             </div>
-            <article>
-              <h3>{data[index]?.name}</h3>
-              <ul>
-                <li>
-                  <span>Native Name:</span> {data[index]?.nativeName}
-                </li>
-                <li>
-                  <span>Population:</span> {data[index]?.population}
-                </li>
-                <li>
-                  <span>Region:</span> {data[index]?.region}
-                </li>
-                <li>
-                  <span>Sub Region:</span> {data[index]?.subregion}
-                </li>
-                <li>
-                  <span>Capital:</span> {data[index]?.capital}
-                </li>
-                <li>
-                  <span>Top Level Domain:</span> {data[index]?.topLevelDomain}
-                </li>
-                <li>
-                  <span>Currencies:</span>{" "}
-                  {data[index]?.currencies?.map((curr) => (
-                    <span key={curr?.name}>{curr?.name}, </span>
-                  ))}
-                </li>
-                <li>
-                  <span>Languages:</span>{" "}
-                  {data[index]?.languages?.map((lang) => (
-                    <span key={lang?.name}>{lang?.name}, </span>
-                  ))}
-                </li>
+            <article className="stack-md country-info">
+              <h3 className="title">{data[index]?.name}</h3>
+              <ul className="info-list switcher">
+                <div className="list-group stack-sm">
+                  <li>
+                    <span>Native Name:</span> {data[index]?.nativeName}
+                  </li>
+                  <li>
+                    <span>Population:</span> {data[index]?.population}
+                  </li>
+                  <li>
+                    <span>Region:</span> {data[index]?.region}
+                  </li>
+                  <li>
+                    <span>Sub Region:</span> {data[index]?.subregion}
+                  </li>
+                  <li>
+                    <span>Capital:</span> {data[index]?.capital}
+                  </li>
+                </div>
+                <div className="list-group stack-sm">
+                  <li>
+                    <span>Top Level Domain:</span> {data[index]?.topLevelDomain}
+                  </li>
+                  <li>
+                    <span>Currencies:</span>{" "}
+                    {data[index]?.currencies?.map((curr) => (
+                      <span key={curr?.name}>{curr?.name}, </span>
+                    ))}
+                  </li>
+                  <li>
+                    <span>Languages:</span>{" "}
+                    {data[index]?.languages?.map((lang) => (
+                      <span key={lang?.name} className="unbold">
+                        {lang?.name},{" "}
+                      </span>
+                    ))}
+                  </li>
+                </div>
               </ul>
-              <div>
-                Border Countries:
-                <div>
+              <div className="cluster flex-start-x">
+                <span>Border Countries:</span>
+                <div className="cluster flex-start-x">
                   {data[index]?.borders?.map((border) => (
                     <Link
                       to={`../../countries/${findIndex(
@@ -67,6 +73,7 @@ const CountryDetails = () => {
                         "alpha3Code",
                         border
                       )}`}
+                      className="button button--link"
                     >
                       {border}
                     </Link>
