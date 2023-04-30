528c82184e5a30023fbf69b7c53b3913a8ba3cae
adejolx <recursr.dev@gmail.com>
2023-04-30 18:08:24 +0100

    fix: add optional flag to rendered properties
    This will optionally render a property if it is likely to be undefined

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -20,45 +20,45 @@ const CountryDetails = () => {
         </div>
         <Wrapper>
           <div>
-            <img src={data[index].flags.svg} />
+            <img src={data[index]?.flags?.svg} />
           </div>
           <article>
-            <h3>{data[index].name}</h3>
+            <h3>{data[index]?.name}</h3>
             <ul>
               <li>
-                <span>Native Name:</span> {data[index].nativeName}
+                <span>Native Name:</span> {data[index]?.nativeName}
               </li>
               <li>
-                <span>Population:</span> {data[index].population}
+                <span>Population:</span> {data[index]?.population}
               </li>
               <li>
-                <span>Region:</span> {data[index].region}
+                <span>Region:</span> {data[index]?.region}
               </li>
               <li>
-                <span>Sub Region:</span> {data[index].subregion}
+                <span>Sub Region:</span> {data[index]?.subregion}
               </li>
               <li>
-                <span>Capital:</span> {data[index].capital}
+                <span>Capital:</span> {data[index]?.capital}
               </li>
               <li>
-                <span>Top Level Domain:</span> {data[index].topLevelDomain}
+                <span>Top Level Domain:</span> {data[index]?.topLevelDomain}
               </li>
               <li>
                 <span>Currencies:</span>{" "}
-                {data[index].currencies.map((curr) => (
-                  <span key={curr.name}>{curr.name},</span>
+                {data[index]?.currencies?.map((curr) => (
+                  <span key={curr?.name}>{curr?.name},</span>
                 ))}
               </li>
               <li>
                 <span>Languages:</span>{" "}
-                {data[index].languages.map((lang) => (
-                  <span key={lang.name}>{lang.name},</span>
+                {data[index]?.languages?.map((lang) => (
+                  <span key={lang?.name}>{lang?.name},</span>
                 ))}
               </li>
             </ul>
             <div>
               Border Countries:
-              {data[index]?.borders.map((border) => (
+              {data[index]?.borders?.map((border) => (
                 <Link
                   to={`../../countries/${findIndex(
                     data,
